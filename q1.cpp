0767f5b956656fba7ac27e164b42d07ed74a98ee
Anamay Singh <anaamaysingh@gmail.com>
2022-10-10 17:36:50 -0700

    6.1 Question 1

--- q1.cpp
+++ q1.cpp
@@ -5,17 +5,17 @@ using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
-void swapTwoValues(int &n1, int &n2);
-void swapTwoValues(float &f1, float &f2);
+
 void getinput(int &n1, int &n2);
 void getinput(float &f1, float &f2);
-
-
+void swapTwoValues(int &n1, int &n2);
+void swapTwoValues(float &f1, float &f2);
 
 int main()
 {
 	int num1, num2;
 	float fnum1, fnum2;
+
 	getinput(num1, num2);
 	cout << " Before Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
 	swapTwoValues(num1, num2);
@@ -32,29 +32,23 @@ int main()
 // Implement all your functions here
 // ******************************
 
-void getInput(int &n1, int &n2) {
-	cout << "type two numbers" << endl;
+void getinput(int &n1, int &n2) {
+	cout << "Type two integers that need to be swapped:" << endl;
 	cin >> n1 >> n2;
 }
 
-void getInput(float &f1, float &f2) {
-	cout << "type two numbers" << endl;
+void getinput(float &f1, float &f2) {
+	cout << "Type two float numbers that need to be swapped:" << endl;
 	cin >> f1 >> f2;
 }
 
-void swapTwoValues(int n1, int n2) {
-
+void swapTwoValues(int &n1, int &n2) {
 	int stable = n1;
-
 	n1 = n2;
-
 	n2 = stable;
-
 }
-void swapTwoValues(float f1, float f2) {
-
+void swapTwoValues(float &f1, float &f2) {
 	float stable = f1;
-
-	f1 = 
-
-}
+	f1 = f2;
+	f2 = stable;
+} 
